021abdc4e377d4fd1a1b69c3419130b065f0e0a2
armi1189 <armi1189@gmail.com>
2015-06-27 15:12:05 +0200

    added balance to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :default_values
+  def default_values
+    self.balance ||= 0
+  end
 end
