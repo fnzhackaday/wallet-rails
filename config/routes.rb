13c158bdfdbf01d869c4df507f0d2b8e97c86a71
GJMcGowan <gemcgow@gmail.com>
2015-06-27 13:41:57 +0200

    Making stripe work

--- config/routes.rb
+++ config/routes.rb
@@ -58,5 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root to: "transactions#index"
+  # root to: "transactions#index"
 end
