9e6fc591cfc1e2e8d5732cc6cd45431d6e102f12
GJMcGowan <gemcgow@gmail.com>
2015-06-27 17:53:24 +0200

    Doesn't fail badly on error

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,16 +5,20 @@ class TransactionsController < ApplicationController
 
   def create
     @transaction = current_user.transactions.new(transaction_params)
-    id = User.where(email: @transaction.recipient_id).first.id
-    @transaction.recipient_id = id
-    if @transaction.save
-      recipient = User.find(@transaction.recipient_id)
-      recipient_old_balance = recipient.balance
-      recipient_new_balance = recipient_old_balance + @transaction.amount
-      recipient.update(balance: recipient_new_balance)
-      old_balance = current_user.balance
-      new_balance = old_balance - @transaction.amount
-      current_user.update(balance: new_balance)
+    if User.where(email: @transaction.recipient_id).first
+      id = User.where(email: @transaction.recipient_id).first.id
+      @transaction.recipient_id = id
+      if @transaction.save
+        recipient = User.find(@transaction.recipient_id)
+        recipient_old_balance = recipient.balance
+        recipient_new_balance = recipient_old_balance + @transaction.amount
+        recipient.update(balance: recipient_new_balance)
+        old_balance = current_user.balance
+        new_balance = old_balance - @transaction.amount
+        current_user.update(balance: new_balance)
+      else
+        "No"
+      end
     else
       "No"
     end
