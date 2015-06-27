92a39992d0497ef36be0022b01ed06777f20ae0a
GJMcGowan <gemcgow@gmail.com>
2015-06-27 13:46:02 +0200

    Green stripe test

--- spec/features/charges_feature_spec.rb
+++ spec/features/charges_feature_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'helpers/charges_helper.rb'
 
 feature 'stripe' do
 
@@ -9,10 +10,13 @@ feature 'stripe' do
   end
 
   context 'if the user can pay' do
+    before do
+      visit('/charges/new')
+    end
 
     scenario 'fills in and submits stripe form', js: true, driver: :selenium do
       charge_helper
-      expect(page).to have_content('you paid £ 75 for Campaign!')
+      expect(page).to have_content('Thanks, you paid $5.00!')
     end
   end
 end
